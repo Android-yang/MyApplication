8139de518cb3b1047a25daf62551add0d9e5ea5b
杨科学 <kexue.yang@renren-inc.com>
2019-11-21 10:56:21 +0800

    修改包结构

--- app/src/main/java/com/yange/myapplication/widget/recycleview/entity/MultiItemEntity.java
+++
@@ -1,8 +0,0 @@
-package com.yange.myapplication.widget.recycleview.entity;
-
-/**
- * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
- */
-public interface MultiItemEntity {
-    int getItemType();
-}
